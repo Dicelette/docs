655c8323d22f93aee45bd6c0d059531868de1259
Mara <mara-li@outlook.fr>
2024-03-23 19:22:38 +0100

    adjust first page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,70 +1,69 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import Heading from "@theme/Heading";
+import clsx from "clsx";
+
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Image: string;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+	{
+		title: "Facile d'utilisation",
+		Image: require("@site/static/img/easy.png").default,
+		description: (
+			<>
+			Un simple bot discord qui ne nécessite pas de configuration pour fonctionner ! Il suffit de l'ajouter à votre serveur pour commencer à l'utiliser.
+			</>
+		),
+	},
+	{
+		title: "Fiche de personnage",
+		Image: require("@site/static/img/character.png").default,
+		description: (
+			<>
+        Le bot permet aussi de configurer un modèle, des fiches de personnages et de retenir des dés. Et en plus, vos données restent sur votre serveur !
+			</>
+		),
+	},
+	{
+		title: "Universel",
+		Image: require("@site/static/img/theme-park.png").default,
+		description: (
+			<>
+        Le modèle est adaptable à l'infini, ce qui vous permet de configurer vos fiches pour tout type de roleplay !
+			</>
+		),
+	},
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({title, Image, description}: FeatureItem) {
+	return (
+		<div className={clsx("col col--4")}>
+			<div className="text--center">
+				<img src={Image} className={styles.featureImage} alt={title} width={"100"} height={100}/>
+			</div>
+			<div className="text--center padding-horiz--md">
+				<Heading as="h3">{title}</Heading>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
