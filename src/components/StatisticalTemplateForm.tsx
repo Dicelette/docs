50b0da3e46d2accf3bd132d7195d48fb9c5c847c
Mara-Li <lili.simonetti@outlook.fr>
2024-03-27 16:51:22 +0100

    disable

--- src/components/StatisticalTemplateForm.tsx
+++ src/components/StatisticalTemplateForm.tsx
@@ -61,146 +61,161 @@ const StatisticalTemplateForm: React.FC = () => {
 			>
 				{({ isSubmitting, values }) => (
 					<Form>
-						<h2>Statistiques</h2>
-						<table>
-							<thead>
-								<tr>
-									<th>Nom</th>
-									<th>Valeur minimale</th>
-									<th>Valeur maximale</th>
-									<th>Ou Combinaison</th>
-									<th></th>
-								</tr>
-							</thead>
-							<tbody>
-								<FieldArray name="statistics">
-									{({ push: pushStat, remove: removeStat }) => (
-										<>
-											{values.statistics.map((_, statIndex) => (
-												<tr key={statIndex}>
-													<td>
-														<Field name={`statistics[${statIndex}].name`} />
-													</td>
-													<td>
-														<Field name={`statistics[${statIndex}].min`} />
-													</td>
-													<td>
-														<Field name={`statistics[${statIndex}].max`} />
-													</td>
-													<td>
-														<Field
-															name={`statistics[${statIndex}].combinaison`}
-														/>
-													</td>
-													<td>
-														<button
-															type="button"
-															onClick={() => removeStat(statIndex)}
-														>
-															<FontAwesomeIcon
-																icon="trash"
-																fill="true"
-																height="100"
-																width="100"
-															/>
-														</button>
-													</td>
-												</tr>
-											))}
-											<tr>
-												<td colSpan={5}>
-													<button type="button" onClick={() => pushStat({})}>
-														<FontAwesomeIcon
-															icon="plus"
-															fill="true"
-															height="100"
-															width="100"
-														/>
-													</button>
-												</td>
-											</tr>
-										</>
-									)}
-								</FieldArray>
-							</tbody>
-						</table>
+						<div className="group-other">
+							<div className="diceTotal">
+								<h2> Général </h2>
+								<div>
+									<label htmlFor="total">Total (facultatif) : </label>
+									<Field name="total" type="number" />
+									<ErrorMessage name="total" />
+								</div>
 
-						<h2>Dés types (facultatif)</h2>
-						<table>
-							<thead>
-								<tr>
-									<th>Nom</th>
-									<th>Dés</th>
-									<th> </th>
-								</tr>
-							</thead>
-							<tbody>
-								<FieldArray name="damage">
-									{({ push: pushDamage, remove: removeDamage }) => (
-										<>
-											{values.damage.map((_, damageIndex) => (
-												<tr key={damageIndex}>
-													<td>
-														<Field name={`damage[${damageIndex}].name`} />
-													</td>
-													<td>
-														<Field name={`damage[${damageIndex}].value`} />
-													</td>
-													<td>
-														<button
-															type="button"
-															onClick={() => removeDamage(damageIndex)}
-														>
-															<FontAwesomeIcon
-																icon="trash"
-																fill="true"
-																height="100"
-																width="100"
-															/>
-														</button>
-													</td>
-												</tr>
-											))}
-											<tr>
-												<td colSpan={3}>
-													<button type="button" onClick={() => pushDamage({})}>
-														<FontAwesomeIcon
-															icon="plus"
-															fill="true"
-															height="100"
-															width="100"
-														/>
-													</button>
-												</td>
-											</tr>
-										</>
-									)}
-								</FieldArray>
-							</tbody>
-						</table>
-
-						<div>
-							<label htmlFor="total">Total (facultatif) : </label>
-							<Field name="total" type="number" />
-							<ErrorMessage name="total" />
-						</div>
-
-						<div>
-							<label htmlFor="diceType">Dé majeur (facultatif) : </label>
-							<Field name="diceType" type="text" />
-							<ErrorMessage name="diceType" />
+								<div>
+									<label htmlFor="diceType">Dé majeur (facultatif) : </label>
+									<Field name="diceType" type="text" />
+									<ErrorMessage name="diceType" />
+								</div>
+							</div>
+							<br />
+							<div className="critical">
+								<h3> Valeur critique </h3>
+								<div>
+									<label htmlFor="critical.success">Succès : </label>
+									<Field name="critical.success" type="number" />
+									<ErrorMessage name="critical.success" />
+								</div>
+								<div>
+									<label htmlFor="critical.failure">Échec : </label>
+									<Field name="critical.failure" type="number" />
+									<ErrorMessage name="critical.failure" />
+								</div>
+							</div>
 						</div>
 						<br />
-						<h3> Valeur critique </h3>
-						<div>
-							<label htmlFor="critical.success">Succès : </label>
-							<Field name="critical.success" type="number" />
-							<ErrorMessage name="critical.success" />
+						<div className="statistic">
+							<h2>Statistiques</h2>
+							<table>
+								<thead>
+									<tr>
+										<th>Nom</th>
+										<th>Valeur minimale</th>
+										<th>Valeur maximale</th>
+										<th>Ou Combinaison</th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									<FieldArray name="statistics">
+										{({ push: pushStat, remove: removeStat }) => (
+											<>
+												{values.statistics.map((_, statIndex) => (
+													<tr key={statIndex}>
+														<td>
+															<Field name={`statistics[${statIndex}].name`} />
+														</td>
+														<td>
+															<Field name={`statistics[${statIndex}].min`} 
+																disabled={values.statistics[statIndex].combinaison}/>
+														</td>
+														<td>
+															<Field name={`statistics[${statIndex}].max`} 
+																disabled={values.statistics[statIndex].combinaison}
+															/>
+														</td>
+														<td>
+															<Field
+																name={`statistics[${statIndex}].combinaison`}
+																disabled={values.statistics[statIndex].min || values.statistics[statIndex].max}
+															/>
+														</td>
+														<td>
+															<button
+																type="button"
+																onClick={() => removeStat(statIndex)}
+															>
+																<FontAwesomeIcon
+																	icon="trash"
+																	fill="true"
+																	height="100"
+																	width="100"
+																/>
+															</button>
+														</td>
+													</tr>
+												))}
+												<tr>
+													<td colSpan={5}>
+														<button type="button" onClick={() => pushStat({})}>
+															<FontAwesomeIcon
+																icon="plus"
+																fill="true"
+																height="100"
+																width="100"
+															/>
+														</button>
+													</td>
+												</tr>
+											</>
+										)}
+									</FieldArray>
+								</tbody>
+							</table>
 						</div>
-						<div>
-							<label htmlFor="critical.failure">Échec : </label>
-							<Field name="critical.failure" type="number" />
-							<ErrorMessage name="critical.failure" />
+						<br />
+						<div className="dice-skill">
+							<h3>Dés types (facultatif)</h3>
+							<table>
+								<thead>
+									<tr>
+										<th>Nom</th>
+										<th>Dés</th>
+										<th> </th>
+									</tr>
+								</thead>
+								<tbody>
+									<FieldArray name="damage">
+										{({ push: pushDamage, remove: removeDamage }) => (
+											<>
+												{values.damage.map((_, damageIndex) => (
+													<tr key={damageIndex}>
+														<td>
+															<Field name={`damage[${damageIndex}].name`} />
+														</td>
+														<td>
+															<Field name={`damage[${damageIndex}].value`} />
+														</td>
+														<td>
+															<button
+																type="button"
+																onClick={() => removeDamage(damageIndex)}
+															>
+																<FontAwesomeIcon
+																	icon="trash"
+																	fill="true"
+																	height="100"
+																	width="100"
+																/>
+															</button>
+														</td>
+													</tr>
+												))}
+												<tr>
+													<td colSpan={3}>
+														<button type="button" onClick={() => pushDamage({})}>
+															<FontAwesomeIcon
+																icon="plus"
+																fill="true"
+																height="100"
+																width="100"
+															/>
+														</button>
+													</td>
+												</tr>
+											</>
+										)}
+									</FieldArray>
+								</tbody>
+							</table>
 						</div>
 
 						<button type="submit" disabled={isSubmitting}>
