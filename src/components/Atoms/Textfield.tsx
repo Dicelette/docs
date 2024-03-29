34b3b244a0b3e6f07fcdcb49c62e91c07ee8d118
Medaillek <medaillek@gmail.com>
2024-03-29 14:53:18 +0100

    FIX textField

--- src/components/Atoms/Textfield.tsx
+++ src/components/Atoms/Textfield.tsx
@@ -1,33 +1,43 @@
-import { TextField } from "@mui/material";
-import { Field, FieldProps } from "formik";
-import { FC, HTMLInputTypeAttribute } from "react";
+import { TextField } from '@mui/material'
+import { Field, FieldProps } from 'formik'
+import { FC, HTMLInputTypeAttribute } from 'react'
 
 type TextfieldProps = {
-  autofocus?: boolean;
-  className?: string;
-  label: string;
-  name: string;
-  type?: HTMLInputTypeAttribute;
-};
+	autofocus?: boolean
+	className?: string
+	label: string
+	name: string
+	type?: HTMLInputTypeAttribute
+}
 
-const Textfield: FC<TextfieldProps> = ({ autofocus, className, label, name, type = "text" }) => {
+const Textfield: FC<TextfieldProps> = ({
+	autofocus,
+	className,
+	label,
+	name,
+	type = 'text',
+}) => {
 	return (
-		<Field
-			name={name}
-			render={({ field, form: { errors } }: FieldProps<number>) => {
+		<Field name={name}>
+			{({ field, form: { errors } }: FieldProps<number>) => {
 				return (
 					<TextField
+						value={field.value}
+						onChange={field.onChange}
+						onBlur={field.onBlur}
 						autoFocus={autofocus}
 						className={`!mb-4 w-[400px] ${className}`}
 						error={errors[field.name] !== undefined}
 						label={label}
 						size="small"
+						name={field.name}
 						type={type}
+						typeof={type}
 					/>
-				);
+				)
 			}}
-		/>
-	);
-};
+		</Field>
+	)
+}
 
-export default Textfield;
+export default Textfield
