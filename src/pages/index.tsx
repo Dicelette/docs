8e6cb63ecbb89168901f250d53be8ea801da895f
Mara <mara-li@outlook.fr>
2024-06-15 23:15:00 +0200

    chore: use discord generated links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,36 +9,39 @@ import clsx from "clsx";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-	const {siteConfig} = useDocusaurusContext();
-	return (
-		<header className={clsx("hero hero--primary", styles.heroBanner)}>
-			<div className="container">
-				<Heading as="h1" className="hero__title">
-					{siteConfig.title}
-				</Heading>
-				<p className="hero__subtitle"><Translate id="tagline" description="The tagline for the website">Un bot discord qui lance des dés dans des fils... Et bien plus !</Translate></p>
-				<div className={styles.buttons}>
-					<Link
-						className="button button--secondary button--lg"
-						to="https://discord.com/api/oauth2/authorize?client_id=1182819335878754385&permissions=395137215504&scope=bot+applications.commands">
-						<Translate>Inviter le bot</Translate>
-					</Link>
-				</div>
-			</div>
-		</header>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">
+          <Translate id="tagline" description="The tagline for the website">
+            Un bot discord qui lance des dés dans des fils... Et bien plus !
+          </Translate>
+        </p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="https://discord.com/oauth2/authorize?client_id=1182819335878754385"
+          >
+            <Translate>Inviter le bot</Translate>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default function Home(): JSX.Element {
-	const {siteConfig} = useDocusaurusContext();
-	return (
-		<Layout
-			title={`${siteConfig.title}`}
-			description={siteConfig.tagline}>
-			<HomepageHeader />
-			<main>
-				<HomepageFeatures />
-			</main>
-		</Layout>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`${siteConfig.title}`} description={siteConfig.tagline}>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
 }
